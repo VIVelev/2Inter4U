19d33c9755c7eafbb8818418c858e95782835abb
Anton Yanchev <anton.t.y2002@gmail.com>
2018-03-16 18:05:33 +0200

    menu_tp

--- Desktop Application/main.py
+++ Desktop Application/main.py
@@ -1,10 +1,6 @@
 from tkinter import *
-import os
-from chat import main
 root=Tk()
 
-def chat():
-	main()
 
 root.geometry('1000x500')
 logo = PhotoImage(file="./ht1.png")
@@ -17,23 +13,23 @@ chat_button.place(x=835, y=50)
 
 history_button = Button(root, justify = RIGHT, bg="#b0966b", bd=0)
 history_photo=PhotoImage(file="hstictp.png", width=50, height=50)
-history_button.config(image=history_photo, width=48, height=48)
+history_button.config(image=history_photo, width=46, height=46)
 history_button.place(x=850, y=180)
 
 fav_button = Button(root, justify = RIGHT, bd=0)
-fav_photo=PhotoImage(file="favictp.png", width=47, height=47)
-fav_button.config(image=fav_photo, width=40, height=40)
+fav_photo=PhotoImage(file="favictp.png", width=50, height=50)
+fav_button.config(image=fav_photo, width=46, height=46)
 fav_button.place(x=850, y=250)
 
-aboutus_button = Button(root)
-aboutus_photo = PhotoImage(file="./aboutus.png", width=50, height=50)
-aboutus_button.config(image=aboutus_photo, width=50, height=50)
-aboutus_button.place(x=850, y=390)
+aboutus_button = Button(root, bd=0)
+aboutus_photo = PhotoImage(file="./aboutustp.png", width=50, height=50)
+aboutus_button.config(image=aboutus_photo, width=45, height=45)
+aboutus_button.place(x=845, y=390)
 
-reset_button = Button(root)
-reset_photo = PhotoImage(file="./reset.png", height=50, width=50)
-reset_button.config(image=reset_photo, width=50, height=50)
-reset_button.place(x=850, y=320)
+reset_button = Button(root, bd=0)
+reset_photo = PhotoImage(file="./resettp.png", height=50, width=50)
+reset_button.config(image=reset_photo, width=45, height=45)
+reset_button.place(x=845, y=320)
 
 
 
