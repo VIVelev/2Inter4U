840ec7dfeb1bf3c67e077e6dbf655641b12fce6e
Anton Yanchev <anton.t.y2002@gmail.com>
2018-03-16 11:12:14 +0200

    chat_and_history_buttons

--- Desktop Application/main.py
+++ Desktop Application/main.py
@@ -1,38 +1,20 @@
 from tkinter import *
+root=Tk()
 
-class MyFirstGUI:
-    def __init__(self, master):
-        self.master = master
-        master.title("Your new smart friend!")
+root.geometry('1000x500')
+logo = PhotoImage(file="ht.png")
+background=Label(root, image=logo).place(x=0,y=0,relwidth=1, relheight=1)
 
-        self.label = Label(master, text="Welcome to our project!", font = "Times 16 bold")
-        self.label.pack()
+chat_button=Button(root,justify = LEFT)
+photo_chat=PhotoImage(file="chat1.png", height=100, width=100)
+chat_button.config(image=photo_chat,width=100,height=100)
+chat_button.place(x=850, y=20)
 
-        self.write_button = Button(master, text="Chat",fg="blue",bg ="yellow", font="Times", command=self.greet)
-        self.write_button.pack(pady = 15)
-
-        self.open_button = Button(master, text="History",fg="blue",bg ="yellow", font="Times", command=self.greet)
-        self.open_button.pack(pady = 15)
-
-        self.write_button = Button(master, text="Favourites",fg="blue",bg ="yellow", font="Times", command=self.greet)
-        self.write_button.pack(pady = 15)
-
-        self.open_button = Button(master, text="Reset",fg="blue",bg ="yellow", font="Times", command=self.greet)
-        self.open_button.pack(pady = 15)
+history_button = Button(root, justify = RIGHT)
+photo_history=PhotoImage(file="hstic.png", height=100, width=100)
+history_button.config(image=photo_history,width=100, height=100)
+history_button.place(x=850, y=140)
 
 
-        self.open_button = Button(master, text="About us",fg="blue",bg ="yellow", font="Times", command=self.greet)
-        self.open_button.pack(pady = 15)
 
-        self.quit_button = Button(master, text="    QUIT    ", fg="red", font = "Times",bg = "white",  command=quit)
-        self.quit_button.pack(pady = 15)
-
-    def greet(self):
-        print("Greetings!")
-
-
-master = Tk()
-my_gui = MyFirstGUI(master)
-master.title("Your new smart friend")
-master.geometry("900x600")
-master.mainloop()
+root.mainloop()
