f8a1994357fa914c3f8b54f66589d14618b99633
Victor Velev <velev.victor@yahoo.com>
2018-03-16 22:28:06 +0200

    update the summarization algorithm

--- app/engine.py
+++ app/engine.py
@@ -8,6 +8,7 @@ from nlp_utils.methods import (
 )
 
 import wikipedia
+import re
 
 xx=0
 umsg=[]
@@ -49,16 +50,17 @@ class BotBubble:
 	#	self.l3.pack(fill="x")
 		xx+=20
 
-	def recommend (self):
+
+	def recommend(self):
 		global umsg
 		
 		if named_entity_recognition(umsg[-1]):
 			topics = named_entity_recognition(umsg[-1])
-			return summarize_article(
-					wikipedia.page(
-						wikipedia.search(topics[0])[0]
-					).content
-				)
+			print(topics)
+
+			article = wikipedia.page(wikipedia.search(topics[0])[0]).content
+			response = summarize_article(article)
+			return response
 
 		else:
 			last_msg = umsg[-1]
