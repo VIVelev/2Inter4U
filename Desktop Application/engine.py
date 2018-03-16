af1482f35ac8dc83dca2819ad0823a6cc0362a08
Samuil <sami.georgiev@gmail.com>
2018-03-16 13:35:03 +0200

    Engine

--- Desktop Application/engine.py
+++ Desktop Application/engine.py
@@ -10,15 +10,15 @@ class UserBubble:
 		global umsg
 		global bmsg
 		umsg.append(content)
-		self.l1=Label(master,text="Me:",anchor="w",fg="red")
+		self.l1=Label(master,text="Me:\n",anchor="w",fg="red")
 		self.l1.place(x=xx,y=0)
 		self.l1.pack(fill="x")
-		self.l2=Label(master,text=content,anchor="w")
+		self.l2=Label(master,text=content+"\n",anchor="w")
 		self.l2.place(x=xx+10,y=0)
-		self.l2.pack(fill="both")
-	#	self.l3=Label(master,text="",anchor="w",bg="red")
-	#	self.l3.place(x=xx,y=0)
-	#	self.l3.pack(fill="x")
+		self.l2.pack()
+		self.l3=Label(master,text="",anchor="w",bg="red")
+		self.l3.place(x=xx,y=0)
+		self.l3.pack(fill="x")
 		xx+=20
 
 class BotBubble:
@@ -27,21 +27,24 @@ class BotBubble:
 		global umsg
 		global bmsg
 		bmsg.append(content)
-		self.l1=Label(master,text="Bot:",anchor="w",fg="blue")
+		self.l1=Label(master,text="Bot:\n",anchor="w",fg="blue")
 		self.l1.place(x=xx,y=0)
 		self.l1.pack(fill="x")
-		self.l2=Label(master,text=content,anchor="w")
+		self.l2=Label(master,text=content+"\n",anchor="w")
 		self.l2.place(x=xx+10,y=0)
 		self.l2.pack(fill="both")
-	#	self.l3=Label(master,text="",anchor="w",bg="blue")
-	#	self.l3.place(x=xx,y=0)
-	#	self.l3.pack(fill="x")
+		self.l3=Label(master,text="",anchor="w",bg="blue")
+		self.l3.place(x=xx,y=0)
+		self.l3.pack(fill="x")
 		xx+=20
 
-master=Tk()
+def recommend (master):
+	pass
+
+'''master=Tk()
 a=UserBubble(master,"input")
 b=BotBubble(master,"answer")
 master.minsize(width=600, height=600)
 master.maxsize(width=600, height=600)
 print(umsg,bmsg)
-master.mainloop()
+master.mainloop()'''
