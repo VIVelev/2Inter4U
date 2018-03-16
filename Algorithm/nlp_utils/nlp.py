3c81c353b33f4be2cffd0f593f29c564a7339f06
Victor Velev <velev.victor@yahoo.com>
2018-03-16 10:39:47 +0200

    Add noise removing and punct removing

--- Algorithm/nlp_utils/nlp.py
+++ Algorithm/nlp_utils/nlp.py
@@ -1,7 +1,10 @@
 import nltk
+import re
 
 __all__ = (
     "standartize",
+    "remove_punct",
+    "remove_noise",
 )
 
 def standartize(df):
@@ -27,3 +30,36 @@ def standartize(df):
         
         text = " ".join(sents)
         df.iloc[i]["text"] = text
+
+def remove_punct(text):
+    punct = ["`", "(\!)", "@", "#", "(\$)", "%", "(\^)", "(\&)", "(\*)", "(\()", "(\))", "-", "(\+)", "=",
+            "(\{)", "(\})", "(\[)", "(\])", "(\|)", "(\\\\)",
+            ":", ";", "(\")", "(\')",
+            ",", "(\.)", "/", "<", ">", "(\?)",
+            "(\n)", "(\t)"]
+    
+    for p in punct:
+        text = re.sub(p+"+", "", text)
+        
+    text = re.sub("(\s)+", " ", text)
+    return text
+
+def remove_noise(df):
+    stop_words = set(nltk.corpus.stopwords.words("english"))
+    stop_words.remove("not")
+    
+    for i in range(len(df)):
+        text = df.iloc[i]["text"].lower()
+        sents = nltk.sent_tokenize(text)
+
+        for j in range(len(sents)):
+            words = nltk.word_tokenize(sents[j])
+            for word in words:
+                if word in stop_words:
+                    words.remove(word)
+                
+            sents[j] = " ".join(words)
+        
+        text = " ".join(sents)
+        df.iloc[i]["text"] = remove_punct(text)
+        df.iloc[i]["label"] = remove_punct(df.iloc[i]["label"])
