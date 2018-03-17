c32e7a495815065a4949fd46d270504a97885692
georgilubenov <joro2404@abv.bg>
2018-03-17 17:28:13 +0200

    error fixed

--- app/home.py
+++ app/home.py
@@ -21,14 +21,14 @@ def chat():
 		os.system("chat.py 1")
 
 #Making the history button to work
-    def history():
-        platfor = sys.platform
-        if platform == "linux":
-            os.system("python3 histry.py")
-        elif platform == "darwin" :
-            os.system("python3 history.py")
-        else :
-            is.system("history.py")
+def history():
+    platfor = sys.platform
+    if platform == "linux":
+        os.system("python3 histry.py")
+    elif platform == "darwin" :
+        os.system("python3 history.py")
+    else :
+        os.system("history.py")
 
 root = Tk()
 root.geometry('1000x500')
