ce816a4a020020a8de3da2f1a07bc738f894836c
Victor Velev <velev.victor@yahoo.com>
2018-03-17 11:26:48 +0200

    add more comlplicated way to store user data and features for recommendation engine

--- app/engine.py
+++ app/engine.py
@@ -4,14 +4,28 @@ import pandas as pd
 import wikipedia
 import re
 
-from main.methods import predict_emotion
+from main.methods import (
+	predict_emotion,
+	train_recommendation_model,
+	predict_recommendation,
+)
+
 from nlp_utils.methods import (
 	preprocess,
 	summarize_article,
-	named_entity_recognition
+	named_entity_recognition,
+)
+
+from nlp_utils.nlp import (
+	standartize,
+	remove_noise,
+	stem,
 )
 
 DATASET = pd.DataFrame(columns=["text", "label"])
+WIKI_TAGS = pd.DataFrame(columns=["category", "label"])
+WIKI_PAGES = []
+
 umsg=[]
 bmsg=[]
 
@@ -56,28 +70,61 @@ class BotBubble:
 
 	def recommend(self):
 		global DATASET
+		global WIKI_TAGS
+		global WIKI_PAGES
 		global umsg
 		global bmsg
 		
 		topics = named_entity_recognition(umsg[-1])
+		response = ""
 
 		if len(topics) > 0:
 			print(topics)
 
-			article = wikipedia.page(wikipedia.search(topics[0])[0]).content
-			response = summarize_article(article)
-			return response
+			if len(WIKI_PAGES) == 0:
+				page = wikipedia.page(wikipedia.search(topics[0])[0])
+				WIKI_PAGES.append(page)
+				article = page.content
+				response = summarize_article(article)
+				
+			else:
+				CURRENT_TAGS = WIKI_TAGS
+			
+				standartize(CURRENT_TAGS)
+				remove_noise(CURRENT_TAGS)
+				stem(CURRENT_TAGS)
+
+				X_train, y_train = CURRENT_TAGS["text"], CURRENT_TAGS["label"]
+				print(X_train, y_train)
+
+				response = "HELLO"
 
 		elif len(bmsg) > 1:
-			last_msg = umsg[-1]
-			X_tf = preprocess(last_msg)
+			last_umsg = umsg[-1]
+
+			X_tf = preprocess(last_umsg)
 			label = predict_emotion(X_tf)
+
+			WIKI_TAGS = WIKI_TAGS.append(
+				pd.DataFrame(
+					[
+						[" ".join(WIKI_PAGES[-1].categories), label]
+					],
+					columns=["category", "label"]
+				),
+				ignore_index=True
+			)
+
 			DATASET = DATASET.append(
 				pd.DataFrame([[bmsg[-1], label]], columns=["text", "label"]),
 				ignore_index=True
 			)
+		
+		else:
+			pass
 
-		print(DATASET)		
+		print(WIKI_TAGS)		
+		return response
 
 
 '''master=Tk()
--- app/main/methods.py
+++ app/main/methods.py
@@ -1,10 +1,32 @@
 import pickle
+from .algo import MyModel
+
+from sklearn.naive_bayes import BernoulliNB
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
 
 __all__ = [
     "predict_emotion",
+    "train_recommendation_model",
+    "predict_recommendation",
 ]
 
 sentiment_algo = pickle.load(open("./main/sentiment_algo.pickle", "rb"))
 
 def predict_emotion(X):
     return sentiment_algo.predict(X)[0]
+
+def train_recommendation_model(X_train, y_train):
+    model = MyModel(
+        BernoulliNB(alpha=1.0),
+        BernoulliNB(alpha=1.0),
+        LogisticRegression(C=1.0, dual=True),
+        DecisionTreeClassifier(max_depth=100),
+    )
+
+    model.fit(X_train, y_train)
+
+    return model    
+
+def predict_recommendation(model, X_test):
+    return model.predict(X_test)[0]
