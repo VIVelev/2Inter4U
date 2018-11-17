61651794512bbdce59d3f22c7afec51a74971a08
Victor Velev <velev.victor@yahoo.com>
2018-11-17 20:09:10 +0200

    [Update] replace scikit-learn with pydojoml

--- src/ml/main/__init__.py
+++
@@ -1,7 +0,0 @@
-import main.algo
-import main.methods
-
-__all__ = (
-    algo.__all__,
-    methods.__all__,
-)
--- src/ml/nlp_utils/__init__.py
+++
@@ -1,7 +0,0 @@
-import nlp_utils.nlp
-import nlp_utils.methods
-
-__all__ = (
-    nlp.__all__,
-    methods.__all__,
-)
