12aabaa17f36cf6ba8499b6b01bf93375f22903c
Victor Velev <velev.victor@yahoo.com>
2018-11-18 11:32:38 +0200

    [Update] lemmatizing

--- src/models/nlp/preprocessing.py
+++ src/models/nlp/preprocessing.py
@@ -79,19 +79,16 @@ def remove_noise(df):
 def lemmatize(df):
     lemmatizer = nltk.WordNetLemmatizer()
     
-    for i in range(len(df)):
-        text = df.iloc[i]["text"].lower()
-        sents = nltk.sent_tokenize(text)
-        
-        for j in range(len(sents)):
-            words = nltk.word_tokenize(sents[j])
-            for k in range(len(words)):
-                words[k] = lemmatizer.lemmatize(words[k])
-                
-            sents[j] = " ".join(words)
-        
-        text = " ".join(sents)
-        df.iloc[i]["text"] = text
+    corpus = ['' for _ in range(df.shape[0])]
+    i = 0
+
+    for sample in df.values:
+        for sent in nltk.sent_tokenize(sample[0].lower()):
+            for word in nltk.word_tokenize(sent):
+                corpus[i] += lemmatizer.lemmatize(word) + ' '
+        i+=1
+
+    df["text"] = corpus
 
 
 def stem(df):
