a333e1de714c1cafc1e7a7658372ebaccc185e52
Victor Velev <velev.victor@yahoo.com>
2018-11-29 12:37:21 +0200

    [Update] better recommendation system

--- src/bot.py
+++ src/bot.py
@@ -1,6 +1,8 @@
 import random as rnd
 import wikipedia
 
+from dojo.linear import LinearRegression
+
 from models.main import (
     get_named_entities,
     get_nouns,
@@ -21,11 +23,22 @@ class Bot:
     def __init__(self, name):
         self.name = name
 
-        self.pages_stats = {}
+        self.pages_score = {}
+        self.recommender_system = LinearRegression()
         self.prev_page = None
     
-    def page_similarity(self, pageA, pageB):
-        pass
+    def calc_page_score(self, page2analyze):
+        wiki_pages = [wikipedia.page(title) for title in self.pages_score.keys()]
+        X = [
+            [len(get_named_entities(page.content)), len(get_nouns(page.content))] for page in wiki_pages
+        ]
+        y = self.pages_score.values()
+
+        self.recommender_system.fit(X, y)
+        return self.recommender_system.predict(
+            [len(get_named_entities(page2analyze.content)),
+            len(get_nouns(page2analyze.content))]
+        )
 
     def get_search_string(self, msg):
         search_string = ' '.join([ent[0] for ent in get_named_entities(msg)])
@@ -66,7 +79,7 @@ class Bot:
 
         sentiment = get_sentiment(msg)[0][1]
         log(msg, ":", sentiment)
-        self.pages_stats[self.prev_page.title] = sentiment
+        self.pages_score[self.prev_page.title] = sentiment
 
         res = "Thanks for the feedback. I recorder that you "
         if sentiment < .5:
